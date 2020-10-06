b246324da91311bafa35cbe8f040d84821681c3f
kenchon <kenh5526@gmail.com>
2020-10-06 23:56:31 +0900

    feat: Realize dynamic clock

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { cleanup } from '@testing-library/react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 const Clock:React.FC = () => {
+
+    const [currentTime, setCurrentTime] = useState(() => new Date())
+    useEffect(() => {
+        let timerId = setInterval(()=>tick(),1000);
+        return function cleanup(){
+            clearInterval(timerId);
+        };
+    })
+
+    function tick() {
+        setCurrentTime(new Date());
+    }
+
     return (
         <StyledClock>
-            <span>{Date()}</span>
+            <span>{currentTime.toLocaleTimeString()}</span>
         </StyledClock>
     )
 }
@@ -12,6 +26,7 @@ const Clock:React.FC = () => {
 export default Clock
 
 const StyledClock = styled.div`
+    font-size: 50px;
     font-weight: 900;
     z-index: 2;
     top: 0;
@@ -23,4 +38,8 @@ const StyledClock = styled.div`
     align-self: 'absolute';
     align-items: 'center';
     justify-content: 'center';
+    text-shadow: 1px 1px 0 #FFF, -1px -1px 0 #FFF,
+              -1px 1px 0 #FFF, 1px -1px 0 #FFF,
+              0px 1px 0 #FFF,  0-1px 0 #FFF,
+              -1px 0 0 #FFF, 1px 0 0 #FFF;
 `
