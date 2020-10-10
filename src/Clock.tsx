4d26f5201d101db30c01fa0fd4153f5ce4d293fe
kenchon <kenh5526@gmail.com>
2020-10-10 23:05:10 +0900

    feat: Centering clock string

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -2,6 +2,8 @@ import { cleanup } from '@testing-library/react'
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
+const imgHeight = window.innerHeight
+
 const Clock:React.FC = () => {
 
     const [currentTime, setCurrentTime] = useState(() => new Date())
@@ -26,20 +28,19 @@ const Clock:React.FC = () => {
 export default Clock
 
 const StyledClock = styled.div`
-    font-size: 50px;
+    font-family: Courier New;
+    font-size: 150px;
     font-weight: 900;
     z-index: 2;
-    top: 0;
+    top: ${imgHeight/2 - 80}px;
     left: 0;
     right: 0;
     bottom: 0;
-    position: absolute;
+    position: absolute; /* necessary for overlay*/ 
     flex: 1;
     align-self: 'absolute';
-    align-items: 'center';
-    justify-content: 'center';
-    text-shadow: 1px 1px 0 #FFF, -1px -1px 0 #FFF,
-              -1px 1px 0 #FFF, 1px -1px 0 #FFF,
-              0px 1px 0 #FFF,  0-1px 0 #FFF,
-              -1px 0 0 #FFF, 1px 0 0 #FFF;
+    text-shadow: 2px 2px 0 #FFF, -2px -2px 0 #FFF,
+              -2px 2px 0 #FFF, 2px -2px 0 #FFF,
+              0px 2px 0 #FFF,  0-2px 0 #FFF,
+              -2px 0 0 #FFF, 2px 0 0 #FFF;
 `
