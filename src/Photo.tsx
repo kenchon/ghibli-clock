7f683287dce1bf8b67e34a46b19e815441a4c13e
kenchon <kenh5526@gmail.com>
2020-10-13 23:09:03 +0900

    feat: Add feature to update background img every 60 sec

--- src/Photo.tsx
+++ src/Photo.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Clock from './Clock'
 
 const Photo: React.FC = () => {
 
-    const imgWidth = window.innerWidth
-    const imgHeight = window.innerHeight
-    const imgSrc = `https://loremflickr.com/${imgWidth}/${imgHeight}/cat`
+    let imgWidth = window.innerWidth
+    let imgHeight = window.innerHeight
+    let imgSrc = `https://loremflickr.com/${imgWidth}/${imgHeight}/cat`
+
+    // TODO: Clock でグローバルな currentTime を定義して，これを参照するようにする
+    // TODO: currentImgSrc の値を変えない方法で，<img> を再描画する方法を使用する
+    const [currentTime, setCurrentTime] = useState(() => new Date())
+    const [currentImgSrc, setCurrentImgSrc] = useState(()=> `https://loremflickr.com/${imgWidth}/${imgHeight}/cat`)
+
+    useEffect(() => {
+        let timerId = setInterval(()=>tick(),1000);
+        return function cleanup(){
+            clearInterval(timerId);
+            if(currentTime.toLocaleTimeString().slice(6, 9) == "00") {
+                setCurrentImgSrc(`https://loremflickr.com/${imgWidth}/${imgHeight}/cat?=${currentTime}`)
+                console.log(`imgSrc: ${imgSrc}`)
+            }
+        };
+    })
+
+    function tick() {
+        setCurrentTime(new Date());
+    }
 
     return (
         <StyledPhoto>
             <Clock />
-            <img width={imgWidth} height={imgHeight} src={imgSrc}/>
+            <img width={imgWidth} height={imgHeight} src={currentImgSrc}/>
         </StyledPhoto>
     )
 }
