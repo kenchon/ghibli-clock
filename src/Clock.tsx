adde3a43d1aa70c97553599bd749a17d2d8ff54c
kenchon <kenh5526@gmail.com>
2020-10-16 23:41:50 +0900

    feat: Add date on clock

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -19,28 +19,49 @@ const Clock:React.FC = () => {
     }
 
     return (
-        <StyledClock>
-            <span>{currentTime.toLocaleTimeString()}</span>
-        </StyledClock>
+        <div>
+            <StyledClock>
+                <ul>
+                    <li><span id="date">{currentTime.toLocaleDateString()}</span></li>
+                    <li><span id="time">{currentTime.toLocaleTimeString()}</span></li>
+                </ul>
+            </StyledClock>
+        </div>
     )
 }
 
 export default Clock
 
 const StyledClock = styled.div`
+    /* font styles */
     font-family: Courier New;
-    font-size: 150px;
     font-weight: 900;
     z-index: 2;
     top: ${imgHeight/2 - 80}px;
+
+    text-shadow: 2px 2px 0 #FFF, -2px -2px 0 #FFF,
+              -2px 2px 0 #FFF, 2px -2px 0 #FFF,
+              0px 2px 0 #FFF,  0-2px 0 #FFF,
+              -2px 0 0 #FFF, 2px 0 0 #FFF;
+
+    #time {
+        font-size: 150px;
+    }
+
+    #date {
+        font-size: 60px;
+    }
+
+    /* positional styles*/
     left: 0;
     right: 0;
     bottom: 0;
     position: absolute; /* necessary for overlay*/ 
     flex: 1;
     align-self: 'absolute';
-    text-shadow: 2px 2px 0 #FFF, -2px -2px 0 #FFF,
-              -2px 2px 0 #FFF, 2px -2px 0 #FFF,
-              0px 2px 0 #FFF,  0-2px 0 #FFF,
-              -2px 0 0 #FFF, 2px 0 0 #FFF;
+
+    ul {
+        list-style: none;
+    }
 `
+
