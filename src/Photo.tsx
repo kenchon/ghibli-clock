8cb0d587eeb4be47f32b3f3e89056048a0f3445b
kenchon <kenh5526@gmail.com>
2020-10-17 11:55:33 +0900

    chore: Add comments for future work

--- src/Photo.tsx
+++ src/Photo.tsx
@@ -10,8 +10,10 @@ const Photo: React.FC = () => {
 
     // TODO: Clock でグローバルな currentTime を定義して，これを参照するようにする
     // TODO: currentImgSrc の値を変えない方法で，<img> を再描画する方法を使用する
+    // TODO: 00秒ちょうどに画像が切り替わるようにする（ƒetchに3秒ぐらい時間がかかる）
     const [currentTime, setCurrentTime] = useState(() => new Date())
     const [currentImgSrc, setCurrentImgSrc] = useState(()=> `https://loremflickr.com/${imgWidth}/${imgHeight}/cat`)
+    const [nextImgSrc, setNextImgSrc] = useState(()=> `https://loremflickr.com/${imgWidth}/${imgHeight}/cat`)
 
     useEffect(() => {
         let timerId = setInterval(()=>tick(),1000);
